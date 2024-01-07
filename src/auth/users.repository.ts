e3af615cd317cf3e4e16c7aae9c4c785665fc7ea
Tihor <tihor.nezic@gmail.com>
2024-01-07 17:44:43 +0100

    Add Sign up

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 // repository is place where you do your complex db interactions
 @Injectable()
 export class UsersRepository extends Repository<User> {
-  //
+  constructor(private dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentials: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentials;
+
+    const user = this.create({ username, password });
+
+    await this.save(user);
+  }
 }
