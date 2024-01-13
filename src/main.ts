66d95ccb61853dead63cbeee075caacdd2becc5d
Tihor <tihor.nezic@gmail.com>
2024-01-13 13:47:26 +0100

    Serialize user data

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +10,8 @@ async function bootstrap() {
   // run validation pipe => it saves us from a lot of code
   // at the controller level
   app.useGlobalPipes(new ValidationPipe());
-
+  // we don't have anymore user information when returning a task
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
 bootstrap();
--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 import { User } from 'src/auth/user.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class Task {
@@ -18,5 +19,8 @@ export class Task {
 
   // you can have many tasks for one user
   @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  // whenever we return the json response, we're going to
+  // exclude the user property
+  @Exclude({ toPlainOnly: true })
   user: User;
 }
